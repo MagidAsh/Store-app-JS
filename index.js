c385ccbfbbdfd0ba2b440d5c8906f5e84ff5a521
Majid Ash <Magid.Ash.Si@gmail.com>
2024-07-29 13:06:59 -0700

    Ability to categorize products

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const dashboardButton = document.getElementById("dashboard");
 const mainContent = document.getElementById("products");
 const searchButton = document.getElementById("search").querySelector("button");
 const inputBox = document.getElementById("search").querySelector("input");
+const listItems = document.querySelectorAll("li");
 
 const showProducts = (products) => {
   mainContent.innerHTML = "";
@@ -66,5 +67,27 @@ const searchHandler = () => {
   showProducts(filteredProducts);
 };
 
+const filterHandler = (event) => {
+  const category = event.target.innerText.toLowerCase();
+
+  listItems.forEach((li) => {
+    if (li.innerText.toLowerCase() === category) {
+      li.className = "selected";
+    } else {
+      li.className = "";
+    }
+  });
+
+  if (category === "all") return showProducts(allProducts);
+
+  const filteredProducts = allProducts.filter(
+    (product) => product.category.toLowerCase() === category
+  );
+  console.log(filteredProducts);
+
+  showProducts(filteredProducts);
+};
+
 document.addEventListener("DOMContentLoaded", init);
 searchButton.addEventListener("click", searchHandler);
+listItems.forEach((li) => li.addEventListener("click", filterHandler));
