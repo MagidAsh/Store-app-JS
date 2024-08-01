2db013bad44f1c9a603137623373090b10774d22
Majid Ash <Magid.Ash.Si@gmail.com>
2024-08-01 05:08:37 -0700

    Show user information on the dashboard page and style it

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,46 @@
 import authHandler from "./utils/authorization.js";
 import { getData } from "./utils/httpReq.js";
 
+const mainContent = document.getElementById("container");
+
+const renderUsers = (users) => {
+  mainContent.innerHTML = "";
+
+  users.forEach((user) => {
+    const jsx = `
+      <div id="card">
+        <h3>${user.id}</h3>
+        <div>
+          <p><i class="fa-solid fa-user"></i>Name:</p>
+          <span>${user.name.firstname} ${user.name.lastname}</span>
+        </div>
+        <div>
+          <p><i class="fa-solid fa-paperclip"></i>Username:</p>
+          <span>${user.username}</span>
+        </div>
+        <div>
+          <p><i class="fa-solid fa-envelope"></i>Email:</p>
+          <span>${user.email}</span>
+        </div>
+        <div>
+          <p><i class="fa-solid fa-phone"></i>Phone:</p>
+          <span>${user.phone}</span>
+        </div>
+        <div>
+          <p><i class="fa-solid fa-location-dot"></i>Address:</p>
+          <span>${user.address.city} - ${user.address.street} - ${user.address.zipcode}</span>
+        </div>
+      </div>
+    `;
+
+    mainContent.innerHTML += jsx;
+  });
+};
+
 const init = async () => {
   authHandler();
-  const user = await getData("users");
-  console.log(user);
+  const users = await getData("users");
+  renderUsers(users);
 };
 
 document.addEventListener("DOMContentLoaded", init);
